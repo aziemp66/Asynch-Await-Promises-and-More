92ccd3629e35568c1a0c8a5762c99330d1497885
Azie Melza Pratama <aziemp66@gmail.com>
2022-01-08 21:45:01 +0700

    Classes & Methods

--- objects.js
+++ objects.js
@@ -13,10 +13,15 @@ class Job {
         this.location = place;
         this.salary = salary;
     }
+    describe() {
+        console.log(
+            `I'm a ${this.title}, I work in ${this.location} and I earn $${this.salary}`
+        );
+    }
 }
 
 const developer = new Job("Developer", "Indonesia", 100000);
 const cook = new Job("Cook", "Malaysia", 75000);
 
-console.log(developer);
-console.log(cook);
+developer.describe();
+cook.describe();
