0345d68b26d0cbc11f54c15a77a23ec1388a96cf
Azie Melza Pratama <aziemp66@gmail.com>
2022-01-08 21:21:52 +0700

    Adding Classes

---
+++ objects.js
@@ -0,0 +1,22 @@
+/* const job = {
+    title: "Developer",
+    location: "New York",
+    salary: 75000,
+};
+
+console.log(new Date().toISOString());
+ */
+
+class Job {
+    constructor(jobTitle, place, salary) {
+        this.title = jobTitle;
+        this.location = place;
+        this.salary = salary;
+    }
+}
+
+const developer = new Job("Developer", "Indonesia", 100000);
+const cook = new Job("Cook", "Malaysia", 75000);
+
+console.log(developer);
+console.log(cook);
