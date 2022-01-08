16c42cec331859e11e2f2eff992ccbaae0503f2d
Azie Melza Pratama <aziemp66@gmail.com>
2022-01-08 21:07:05 +0700

    Error Handling

---
+++ errors.js
@@ -0,0 +1,12 @@
+const fs = require("fs");
+
+function readFile() {
+    try {
+        const fileData = fs.readFileSync("data.json");
+    } catch {
+        console.error("An error occured!");
+    }
+
+    console.log("Hi There!");
+}
+readFile();
