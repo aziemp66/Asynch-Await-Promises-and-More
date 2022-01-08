006bf1484b0250f2b7d978f16b507fefcf0dad31
Azie Melza Pratama <aziemp66@gmail.com>
2022-01-08 23:34:09 +0700

    Asynchronous Error Handling

--- async.js
+++ async.js
@@ -1,12 +1,24 @@
-const fs = require("fs");
+const fs = require("fs/promises");
 
 function readFile() {
-    let fileData;
+    // fs.readFile("data.txt", function (error, fileData) {
+    //     if (error){
+    //     ...
+    //     }
+    //     console.log("File Parsing Done!");
+    //     console.log(fileData.toString());
+    // });
 
-    fs.readFile("data.txt", function (error, fileData) {
-        console.log("File Parsing Done!");
-        console.log(fileData.toString());
-    });
+    fs.readFile("data.txt")
+        .then(function (fileData) {
+            console.log("File Parsing Done");
+            console.log(fileData.toString());
+            //Return anotherAsyncOperations
+        })
+        .then(function () {})
+        .catch(function (error) {
+            console.log(error);
+        });
 
     console.log("Hi There!");
 }
