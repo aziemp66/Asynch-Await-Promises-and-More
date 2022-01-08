b3e8a93fb22aa4d7250adb0bccefb2dc623890c6
Azie Melza Pratama <aziemp66@gmail.com>
2022-01-08 23:46:30 +0700

    Improving code with async / await

--- async.js
+++ async.js
@@ -1,6 +1,7 @@
 const fs = require("fs/promises");
 
-function readFile() {
+async function readFile() {
+    let fileData;
     // fs.readFile("data.txt", function (error, fileData) {
     //     if (error){
     //     ...
@@ -8,18 +9,15 @@ function readFile() {
     //     console.log("File Parsing Done!");
     //     console.log(fileData.toString());
     // });
+    try {
+        fileData = await fs.readFile("data.txt");
+    } catch (error) {
+        console.log(error);
+    }
 
-    fs.readFile("data.txt")
-        .then(function (fileData) {
-            console.log("File Parsing Done");
-            console.log(fileData.toString());
-            //Return anotherAsyncOperations
-        })
-        .then(function () {})
-        .catch(function (error) {
-            console.log(error);
-        });
-
+    console.log("File Parsing Done");
+    console.log(fileData.toString());
+    //Return anotherAsyncOperations
     console.log("Hi There!");
 }
 readFile();
